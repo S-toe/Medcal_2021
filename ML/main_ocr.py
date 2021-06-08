8b07f427e71cb9af50600f60140f3e885e5ede2a
naufalrzql0 <naufalriz123@gmail.com>
2021-06-08 17:25:32 +0700

    update main_ocr

--- ML/main_ocr.py
+++ ML/main_ocr.py
@@ -2,9 +2,9 @@ import cv2
 import numpy as np
 import pytesseract
 
-#Parameter
+#Model Parameter
 per = 25
-pixelThershold = 500
+PixThers = 500
 roi = [[(160, 860), (664, 928), 'text', 'med1'], 
        [(162, 942), (664, 1000), 'text', 'med2'], 
        [(164, 1008), (660, 1066), 'text', 'med3'], 
@@ -15,26 +15,23 @@ roi = [[(160, 860), (664, 928), 'text', 'med1'],
        [(154, 1520), (660, 1580), 'text', 'med8']]
 
 # Open the image files.
-img1_color = cv2.imread('custom_receipt.png')  # Image to be aligned.
-img2_color = cv2.imread('custom_receipt.png')    # Reference image.
+imgA_color = cv2.imread('custom_receipt.png')  # Image to be aligned.
+imgR_color = cv2.imread('custom_receipt.png')    # Reference image.
   
 # Convert to grayscale.
-img1 = cv2.cvtColor(img1_color, cv2.COLOR_BGR2GRAY)
-img2 = cv2.cvtColor(img2_color, cv2.COLOR_BGR2GRAY)
-height, width = img2.shape
+imgA = cv2.cvtColor(imgA_color, cv2.COLOR_BGR2GRAY)
+imgR = cv2.cvtColor(imgR_color, cv2.COLOR_BGR2GRAY)
+height, width = imgR.shape
 
 # Create ORB detector with 5000 features.
 orb_detector = cv2.ORB_create(5000)
   
 # Find keypoints and descriptors.
-# The first arg is the image, second arg is the mask
-#  (which is not reqiured in this case).
-kp1, d1 = orb_detector.detectAndCompute(img1, None)
-kp2, d2 = orb_detector.detectAndCompute(img2, None)
+kp1, d1 = orb_detector.detectAndCompute(imgA, None)
+kp2, d2 = orb_detector.detectAndCompute(imgR, None)
 
 # Match features between the two images.
-# We create a Brute Force matcher with 
-# Hamming distance as measurement mode.
+# We create a Brute Force matcher with Hamming distance as measurement mode.
 matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
 
 # Match the two sets of descriptors.
@@ -58,29 +55,27 @@ for i in range(len(matches)):
 # Find the homography matrix.
 homography, mask = cv2.findHomography(p1, p2, cv2.RANSAC)
 
-transformed_img = cv2.warpPerspective(img1_color,
+transformed_img = cv2.warpPerspective(imgA_color,
                     homography, (width, height))
 #cv2_imshow(transformed_img)
 
-imgShow = transformed_img.copy()
-imgMask = np.zeros_like(imgShow)
+img_show = transformed_img.copy()
+img_mask = np.zeros_like(img_show)
 
 myData = []
 
-#print(f'====================== Extracting Data From Receipt ===================================')
-
 for x, r in enumerate(roi):
 
-    cv2.rectangle(imgMask, ((r[0][0]), r[0][1]), ((r[1][0]), r[1][1]), (0, 255, 0), cv2.FILLED)
-    imgShow = cv2.addWeighted(imgShow, 0.99, imgMask, 0.1, 0)
+    cv2.rectangle(img_mask, ((r[0][0]), r[0][1]), ((r[1][0]), r[1][1]), (0, 255, 0), cv2.FILLED)
+    img_show = cv2.addWeighted(img_show, 0.99, img_mask, 0.1, 0)
 
-    imgCrop = transformed_img[r[0][1]:r[1][1], r[0][0]:r[1][0]]
-    #cv2_imshow(imgCrop)
-    imgGray = cv2.cvtColor(imgCrop, cv2.COLOR_BGR2GRAY)
-    imgThresh = cv2.threshold(imgGray, 170, 255, cv2.THRESH_BINARY_INV)[1]
-    totalPixels = cv2.countNonZero(imgThresh)
+    img_crop = transformed_img[r[0][1]:r[1][1], r[0][0]:r[1][0]]
+    #cv2_imshow(img_crop)
+    img_gray = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
+    img_thresh = cv2.threshold(img_gray, 170, 255, cv2.THRESH_BINARY_INV)[1]
+    Pix = cv2.countNonZero(img_thresh)
 
     if r[2] == 'text':
-        if totalPixels > pixelThershold: 
-            print(f'{r[3]} : {pytesseract.image_to_string(imgCrop)}')
-            myData.append(pytesseract.image_to_string(imgCrop)) #bisa diganti sama model nn yang lain
+        if Pix > PixThers: 
+            print(f'{r[3]} : {pytesseract.image_to_string(img_crop)}')
+            myData.append(pytesseract.image_to_string(img_crop)) #can be changed with another model
